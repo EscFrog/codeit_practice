908ea86cceee5637099dff5bd4d89e1f702500c7
Walter Park <neodust223@gmail.com>
2020-11-19 00:34:19 +0900

    Update script.js

--- Streming Site/js/script.js
+++ Streming Site/js/script.js
@@ -14,16 +14,27 @@ function scrollHandler() {
     */
 
     $('.playlist').each(function(){
-        console.log('lslsl');
         let thisHalf = $(this).position().top + $(this).outerHeight() / 2;  // 각 플레이 리스트의 맨 위 위치에다가 플레이 리스트의 높이를 더한 후 2로 나누면 중간을 알 수 있다. 위치와 높이를 jquery함수로 가져왔다.
       
         if (thisHalf < windowBottom) {    // 플레이리스트의 중간이 윈도우의 맨 아래보다 크면, 즉 걸치면.
-            $(this).animate({'opacity': '1'}, 1000);   // 1초간 투명도가 1로 변하는 애니메이션 실행
+          $(this).animate({'opacity': '1'}, 1000);   // 1초간 투명도가 1로 변하는 애니메이션 실행
         }
     })
+
+    // 스크롤이 가장 아래까지 내려오면 버튼이 나타났다가, 올라가면 사라진다
+    if ($(document).height() == windowBottom) {
+      $('.to-top-btn').fadeIn(500);
+    } else {
+      $('.to-top-btn').fadeOut(500);
+    }
   }
   
   // 스크롤 될 때마다 scrollHandler() 함수 호출
   $(window).on('scroll', scrollHandler);
   // 처음에도 한 번 scrollHandler() 함수 호출되도록
   $(document).ready(scrollHandler());
+
+  // to-top-btn을 누르면 가장 위로 이동
+  $('.to-top-btn').on('click', function () {
+    $('html, body').animate({scrollTop: 0}, 1000);
+  })
